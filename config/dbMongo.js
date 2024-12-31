4dfcc3a77e8d1f86af34a8fc6d97fce09d138f5d
Sohaib Asghar <sohaib@mavrictech.com>
2024-12-31 18:13:14 +0500

    mongo config changed

--- config/dbMongo.js
+++ config/dbMongo.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://sohaibasghar1997:Root123@cluster0.civkd.mongodb.net/meow?retryWrites=true&w=majority', {
-      useUnifiedTopology: true, // Optional: still recommended
-    });
+    const conn = await mongoose.connect('mongodb+srv://sohaibasghar1997:Root123@cluster0.civkd.mongodb.net/meow?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
